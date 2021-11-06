fdd2fd1e3d6ab65998031c88cec531ef5d9602e0
BareaMartin <martin.barea94@gmail.com>
2021-11-06 15:03:23 -0300

    added endpoint to createNewPsychologist

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const psychologistsController = require('./controllers/psychologists');
 
 app.set('json spaces', 2);
 
+//Init body parser middleware
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
   res.send('Mindset frontpage');
 });
 
-//ADMIN
+//ADMIN psychologists
 app.get('/psychologists', psychologistsController.getAll);
 app.get('/psychologists/byId/:id', psychologistsController.getById);
 app.get('/psychologists/byName/:name', psychologistsController.getByName);
-app.get('/psychologists/update/:id', psychologistsController.update);
+app.post('/psychologists/new', psychologistsController.createPsychologist);
 
 app.listen(port, () => {
-  console.log(`MindSet server listening at http://localhost:${port}`);
-});
+    console.log(`MindSet server listening at http://localhost:${port}`);
+  });
