ba68c85123fdb77b0b13504cfd7068d7ed34ca8b
BareaMartin <martin.barea94@gmail.com>
2021-11-06 18:17:45 -0300

    first commit - Added state change for applicants

---
+++ controllers/applicants.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./data/applicants.json');
+const applicants = JSON.parse(data);
+
+const getAll = (req, res) => {
+    res.json(applicants);
+  };
+
+// Single applicant by ID
+const getById = (req, res) => {
+    const found = applicants.some(applicant => applicant.id === parseInt(req.params.id));
+    if (found) {
+      res.json(applicants.filter(applicant => applicant.id === parseInt(req.params.id)));
+    } else {
+      res.send('Applicant not found');
+    }
+  };
+
+  //Update applicant
+const updateApplicant = (req, res) => {
+    const found = applicants.some(applicant => applicant.id === parseInt(req.params.id));
+    if (found) {
+      const updateApplicant = req.body;
+      applicants.forEach(applicant => {
+        if(applicant.id === parseInt(req.params.id)){
+          applicant.state = updateApplicant.state
+        }});
+    } else {
+      res.send('Applicant not found');
+    }
+};
+
+  module.exports = {
+    getAll: getAll,
+    getById: getById,
+    updateApplicant: updateApplicant,
+}
