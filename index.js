f10ca2c9b061a85eafa6544d91f2ec2791ac8b72
Sabrina <pereira.tsabrina@gmail.com>
2021-11-06 12:26:13 -0300

    book an appointment

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = process.env.port || 5000;;
 app.listen(port, ()=>
 console.log("server started on port 5000"))
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 //Postulante - Reservar, ver y cancelar un turno de entrevista con un Psicólogo de MindSet.
 
 //Look interview
@@ -33,3 +37,27 @@ app.delete("/psychologist-interviews/:id",(req, res)=>{
   }
 })
 
+//Book an appointment
+
+app.post("/psychologist-interviews", (req, res)=>{
+  const newInterview = {
+    id: req.body.id,
+    appointmentDay: req.body.appointmentDay,
+    appointmentTime: req.body.appointmentTime
+  }
+
+  if(!newInterview.id || !newInterview.appointmentDay || !newInterview.appointmentTime){
+    return res.status(400).json({msg: "Incomplete Data"});
+  }
+
+  const found = psychAppointments.some(interview => interview.id ===parseInt(req.body.id))
+  if (found){
+    return res.status(400).json({msg: `There's already an interview booked with the id ${req.body.id}`})
+  }
+
+  psychAppointments.push(newInterview);
+  res.json({
+    msg: `The interview with the id of ${newInterview.id} has been added`,
+    psychAppointments
+  })
+})
