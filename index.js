e6f3e64ee05aefd316e33b6694616cf9717eaa83
Paloma Quiroz <palomaxquiroz@gmail.com>
2021-11-07 18:56:39 -0300

    minor changes for better readability

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const applicantsController = require('./controllers/applicants');
 
 app.use(express.json());
 
+//Create/Register applicant endpoint
 app.post('/register', applicantsController.registerApplicant);
+
+//Upload CV information by new applicant
 app.post('/register/cv', applicantsController.createCV);
+
+//Change availability of applicant
 app.put('/applicants/:id', applicantsController.changeAvailability);
+
 const PORT = 5000;
 app.listen(PORT);
