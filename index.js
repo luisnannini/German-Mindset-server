e0e2c8a84ed92ad1cec770ea33ef85b98ec7295f
BareaMartin <martin.barea94@gmail.com>
2021-11-08 14:09:58 -0300

    fixed indentation resolved conflicts

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const jobOfferController = require('./controllers/jobOffers');
 const applicantsController = require('./controllers/applicants')
 const profileTypesController = require('./controllers/profileTypes')
 
-
-
+//Json visibility
 app.set('json spaces', 2);
 
 //Init body parser middleware
 app.use(express.json());
 
+//Mindset Frontpage
 app.get('/', (req, res) => {
   res.send('Mindset frontpage');
 });
@@ -24,7 +24,7 @@ app.get('/psychologists/:id', psychologistsController.getById);
 app.get('/psychologists/byName/:name', psychologistsController.getByName);
 app.post('/psychologists', psychologistsController.createPsychologist);
 app.delete('/psychologists/:id', psychologistsController.deletePsychologist);
-app.put('psychologists/:id',psychologistsController.updatePsychologist);
+app.put('/psychologists/:id',psychologistsController.updatePsychologist);
 
 //ADMIN Job Offers
 app.get('/job_offers', jobOfferController.getAll);
@@ -34,19 +34,16 @@ app.post('/job_offers', jobOfferController.createJobOffer);
 app.put('/job_offers/:id', jobOfferController.updatejobOffer);
 app.delete('/job_offers/:id', jobOfferController.deletejobOffer);
 
-
 //ADMIN applicants
 app.get('/applicants', applicantsController.getAll);
 app.get('/applicants/:id', applicantsController.getById);
 app.put('/applicants/:id', applicantsController.changeAvailability);
 
-
 //ADMIN Profile Types
 app.get('/profiles', profileTypesController.getAll);
 app.get('/profiles/:id', profileTypesController.getById);
-app.get('/psychologists/byProfile/:profile', profileTypesController.getByName);
-
-
+app.get('/profiles/byProfile/:profile', profileTypesController.getByName);
+app.get('/profiles/anual/:id', profileTypesController.getReport);
 
 app.listen(port, () => {
     console.log(`MindSet server at http://localhost:${port}`);
