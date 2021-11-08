44f5fa048d2cea1b6087ef6e5cab0d10f9000594
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-11-08 10:57:07 -0300

    Index.js update: final blank spaces deleted

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3500;
 
 app.listen(PORT, () => console.log('Server starred on port ${PORT}'));
 
