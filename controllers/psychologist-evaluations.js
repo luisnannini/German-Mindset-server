4e83a65881e289f10c533575fa21b37fe67b8870
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-11-08 19:07:35 -0300

    Psychologist-Evaluations.js added (former Applicants.js)

---
+++ controllers/psychologist-evaluations.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const data = fs.readFileSync('./data/Data_applicants.json');
+const applicants = JSON.parse(data);
+
+//Get all applicants with their scheduled interviews
+
+const getAll = (req, res) => {
+    res.json(applicants);
+  };
+  
+//Update applicant
+
+const updateApplicant = (req, res) => {
+  const found = applicants.some(applicant => applicant.id === parseInt(req.params.id));
+  if (found) {
+    const updateApplicant = req.body;
+    applicants.forEach(applicant => {
+      if(applicant.id === parseInt(req.params.id)){
+        applicant.typeOfProfile = updateApplicant.typeOfProfile ? updateApplicant.typeOfProfile : applicant.typeOfProfile,
+        res.json({ msg: 'Type of profile assigned to applicant', applicant})
+      }}); 
+  } else {
+      res.status(400);
+      res.json({ msg: 'Applicant not found'});
+  }
+};
+
+//Cancel an interview
+const deleteInterview = (req, res) => {
+    const found = applicants.some(applicant => applicant.id === parseInt(req.params.id));
+    if (found) {
+      res.json(applicants.filter(applicant => applicant.id !== parseInt(req.params.id)));
+    } else {
+      res.status(400);
+      res.json({ msg: 'Interview not found'});
+    }
+}
+
+  module.exports = {
+    getAll: getAll,
+    updateApplicant: updateApplicant,
+    deleteInterview : deleteInterview
+}
