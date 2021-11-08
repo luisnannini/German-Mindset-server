b4bc6c45f7ccee9913b46044372f3e5f168ca892
BareaMartin <martin.barea94@gmail.com>
2021-11-08 14:09:36 -0300

    fixed indentations added anual report

--- controllers/profileTypes.js
+++ controllers/profileTypes.js
@@ -10,28 +10,42 @@ const getAll = (req, res) => {
 
 //Single profileType by ID
 const getById = (req, res) => {
-    const found = profileTypes.some(profileType => profileType.id === parseInt(req.params.id));
+    const found = profileTypes.some(profileType => profileType.type.id === parseInt(req.params.id));
     if (found) {
-      res.json(profileTypes.filter(profileType => profileType.id === parseInt(req.params.id)));
-    } else {
-      res.send(400, {"Msg":"Profile type not found"});
-    }
-  };
-
-  
-//Single profileType by profile name
-const getByName = (req, res) => {
-    const found = profileTypes.some(profileType => profileType.profile === req.params.profile);
-    if (found) {
-        res.json(profileTypes.filter(profileType => profileType.profile === req.params.profile));
+      res.json(profileTypes.filter(profileType => profileType.type.id === parseInt(req.params.id)));
     } else {
       res.send(400, {"Msg":"Profile type not found"});
     }
 };
 
+//Single profileType by profile name
+const getByName = (req, res) => {
+    const found = profileTypes.some(profileType => profileType.type.profile === req.params.profile);
+    if (found) {
+        res.json(profileTypes.filter(profileType => profileType.type.profile === req.params.profile));
+    } else {
+      res.send(400, {"Msg":"Profile type not found"});
+    }
+};
+
+//Show anual report
+const getReport = (req, res) => {
+  console.log(req.params.id)
+  const found = profileTypes.some(profileType => profileType.type.id === parseInt(req.params.id));
+  if (found) {
+    profileTypes.forEach(profileType => {
+      if(profileType.type.id === parseInt(req.params.id)) {
+        res.json(`${profileType.type.month}`);
+      }
+    })
+  }else{
+    res.send(400, {"Msg":"Profile type not found"});
+  }
+}
 
 module.exports = {
     getAll: getAll,
     getById: getById,
     getByName: getByName,
+    getReport: getReport
   };
