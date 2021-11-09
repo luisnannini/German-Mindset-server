9d4880a0009203aadb0df7fcf9647fa45c55ffcc
Juan Menchaca <juanmenchaca7698@gmail.com>
2021-11-09 10:28:12 -0300

    Changes in index.js

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const applicantsController = require('./controllers/applicants')
+const applicantsController = require('./controllers/applicants');
+//const psychologists = require('./controllers/psychologists');
 const app = express();
-app.set('json spaces', 2);
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-
-const PORT = process.env.PORT || 3500;
-
-app.listen(PORT, () => console.log(`Server starred on port ${PORT}`));
-
-//Init body parser middleware
+const port = 3000;
 app.use(express.json());
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
-  res.send('Mindset frontpage');
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`)
 });
 
 //Update profile and cancel interviews
  app.get('/applicants', applicantsController.getAll);
+ app.get('/applicants/interview/:id', applicantsController.nextInterview);
  app.put('/applicants/:id', applicantsController.updateApplicant);
- app.delete('/applicants/:id', applicantsController.deleteInterview)
+ app.delete('/applicants/:id', applicantsController.deleteInterview);
+
+//Change type of profile
+
+app.put('/applicants/profiles/:id', applicantsController.updateProfile);
