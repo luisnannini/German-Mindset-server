c4c2019176ea848194b30579691dbd687743a5dd
andrespienizzio <andrespienizzio@gmail.com>
2021-11-14 20:50:40 -0300

    Fix applicants controller

--- controllers/Applicants.js
+++ controllers/Applicants.js
@@ -8,13 +8,13 @@ const getApplicants = (req, res) => {
       .catch((error) =>{
         return res.status(400).json(error)
       })
-  }
+}
 
 const updateApplicants = (req, res) => {
 	Applicants.findByIdAndUpdate(req.params.id,
 	{
         full_name: req.query.full_name,
-	    username: req.query.username,
+        username: req.query.username,
         birth_date: req.query.birth_date,
         phone_number: req.query.phone_number,
         email: red.query.email,
@@ -35,21 +35,21 @@ const updateApplicants = (req, res) => {
 )}
 
 const createApplicants = (req, res) => {
-  const Applicants = new Applicantss({
-    full_name: req.query.full_name,
-	username: req.query.username,
-    birth_date: req.query.birth_date,
-    phone_number: req.query.phone_number,
-    email: red.query.email,
-    address: red.query.address,
-  });
-
-  Applicants.save((error, app) => {
-    if (error) {
-      return res.status(400).json(error);
-    }
-    return res.status(201).json(app);
-  });
+    const Applicants = new Applicantss({
+        full_name: req.query.full_name,
+        username: req.query.username,
+        birth_date: req.query.birth_date,
+        phone_number: req.query.phone_number,
+        email: red.query.email,
+        address: red.query.address,
+      });
+    
+      Applicants.save((error, app) => {
+        if (error) {
+          return res.status(400).json(error);
+        }
+        return res.status(201).json(app);
+      });
 };
 
 const deleteApplicants = (req, res) => {
