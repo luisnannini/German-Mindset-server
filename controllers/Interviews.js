1b07bfab5dfe222f5cc32766402217ad41a84a2e
BareaMartin <martin.barea94@gmail.com>
2021-11-15 11:43:08 -0300

    fixed conflicts

--- controllers/Interviews.js
+++ controllers/Interviews.js
@@ -1,75 +1,35 @@
-// const Interviews = require('../models/Interviews');
+const fs = require('fs');
+const Interviews = require('../models/Interviews');
 
-// const listInterviews = (req, res) => {
-//   Interviews.find()
-//     .then((interviews) => {
-//       res.status(200).json(interviews);
-//     })
-//     .catch((error) => {
-//       res.status(400).json(error);
-//     });
-// };
+// See interviews for Postulant
+const getInterviewsByPostulant = (req, res) => {
+    Interviews.find( {id_user: req.params.id_user} )
+        .then((interviews) => {
+            res.status(200).json(interviews);
+        })
+        .catch((error) => {
+            res.status(404).json(
+                {msg: `There's no interviews booked with the user id ${req.params.id_user}`}
+                )
+        })
+}
 
-// const createInterview = (req, res) => {
-//   const bodyReq = req.body;
-//   const interview = new Interviews({
-//     postulant: bodyReq.postulant,
-//     client: bodyReq.client,
-//     application: bodyReq.application,
-//     status: bodyReq.status,
-//     date: bodyReq.date,
-//     notes: bodyReq.notes,
-//   });
+// Cancel interview
+const deleteInterview = (req, res) => {
+    Interviews.findByIdAndDelete(req.params.id_user, (error, interview) => {
+      if (!interview) {
+        return res.status(404).json(
+            {msg: `There's no interview with the id of ${req.params.id_user}`}
+            ); 
+      }
+      if (error) {
+        return res.status(400).json(error);
+      }
+      return res.status(204).send();
+    });
+  };
 
-//   interview.save((error) => {
-//     if (error) {
-//       return res.status(400).json(error);
-//     }
-//     return res.status(201).json(interview);
-//   });
-// };
-
-// const updateInterview = (req, res) => {
-//   const bodyReq = req.body;
-//   Interviews.findByIdAndUpdate(
-//     req.params.id,
-//     {
-//       postulant: bodyReq.postulant,
-//       client: bodyReq.client,
-//       application: bodyReq.application,
-//       status: bodyReq.status,
-//       date: bodyReq.date,
-//       notes: bodyReq.notes,
-//     },
-//     { new: true },
-//     (error, newInterview) => {
-//       if (!newInterview) {
-//         return res.status(404).json('Interview id does not exist');
-//       }
-
-//       if (error) {
-//         return res.status(400).json(error);
-//       }
-//       return res.status(201).json(newInterview);
-//     },
-//   );
-// };
-
-// const deleteInterview = (req, res) => {
-//   Interviews.findByIdAndDelete(req.params.id, (error, pointedInterview) => {
-//     if (!pointedInterview) {
-//       return res.status(404).json('Interview id does not exist');
-//     }
-//     if (error) {
-//       return res.status(400).json(error);
-//     }
-//     return res.status(204).send();
-//   });
-// };
-
-// module.exports = {
-//   listInterviews,
-//   createInterview,
-//   updateInterview,
-//   deleteInterview,
-// };
+  module.exports = {
+    getInterviewsByPostulant,
+    deleteInterview,
+  }
