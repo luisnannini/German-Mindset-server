3928afdcc05b37531332344b8ff2a69aad78f980
andrespienizzio <andrespienizzio@gmail.com>
2021-11-14 23:00:33 -0300

    Add clients controller

---
+++ controllers/Clients.js
@@ -0,0 +1,100 @@
+const Clients = require('../models/clients');
+
+const getClients = (req, res) => {
+  Clients.find()
+   .then((clients)=>{
+    res.status(200).json(clients)
+   })
+   .catch((error)=>{
+    res.status(400).json(error)
+   })
+}
+
+const getOneClient = (req, res) => {
+  Clients.findById(req.params.id,
+  (error,Clients) => {
+    if(!Clients) {
+      return res.status(404).json({
+        msg: `Client with id: ${req.params.id} was not found`
+      })
+    }
+    if(error) {
+      return res.status(400).json(error)
+    }
+    return res.status(200).json(Clients)
+  })
+}
+
+const updateClient = (req, res) => {
+  Clients.findByIdAndUpdate(req.params.id,
+    { 
+      company_name: req.body.company_name,
+      address: req.body.address,
+      phone_number: req.body.phone_number,
+      email:req.body.email,
+      contact: {
+        full_name: req.body.contact.full_name,
+        phone_number: req.body.contact.phone_number,
+        email: req.body.contact.email,
+        description: req.body.contact.description,
+      }
+    },
+    { new: true }, 
+    (error, updatedClient) => {
+      if(!updatedClient) {
+        return res.status(404).json({
+          msg: `Client with id: ${req.params.id} was not found`
+        })
+      }
+      if(error) {
+        return res.status(400).json(error)
+      }
+      return res.status(200).json(updatedClient)
+    }
+  )
+}
+
+const createClient = (req, res) => {
+  const client = new Client({
+    company_name: req.body.company_name,
+      address: req.body.address,
+      phone_number: req.body.phone_number,
+      email:req.body.email,
+      contact: {
+        full_name: req.body.contact.full_name,
+        phone_number: req.body.contact.phone_number,
+        email: req.body.contact.email,
+        description: req.body.contact.description,
+      }
+  })
+
+  client.save((error, newClient) => {
+    if(error) {
+      return res.status(400).json(error)
+    }
+    return res.status(201).json(newClient)
+  })
+}
+
+const deleteClient = (req,res) => {
+  Clients.findByIdAndDelete(req.params.id,
+  (error, Client) => {
+    if(!Client) {
+      return res.status(404).json({
+        msg: `Client with id: ${req.params.id} was not found`
+      })
+    }
+    if(error) {
+      return res.status(400).json(error)
+    }
+    return res.status(200).json(Client)
+  })
+}
+
+module.exports = {
+  getClients,
+  getOneClient,
+  createClient,
+  updateClient,
+  deleteClient
+}
