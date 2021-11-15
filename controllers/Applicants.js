7f4559c95a34b371e13786b3900cca6e140cf92f
andrespienizzio <andrespienizzio@gmail.com>
2021-11-14 23:05:18 -0300

    Fix createApplicant function in applicants controller

--- controllers/Applicants.js
+++ controllers/Applicants.js
@@ -60,11 +60,11 @@ const createApplicant = (req, res) => {
         address: red.body.address,
       });
     
-      Applicants.save((error, applicant) => {
+      Applicants.save((error, newApplicant) => {
         if (error) {
         return res.status(400).json(error);
         }
-        return res.status(201).json(applicant);
+        return res.status(201).json(newApplicant);
       });
 };
 
