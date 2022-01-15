a4f62adb69550bf3d2babe3fe1ec60e16aabd03b
agustin-schiaffino <schiaffinoagustin95@gmail.com>
2022-01-15 17:21:31 -0300

    add password validations to the to the auth validations file

--- validations/auth.js
+++ validations/auth.js
@@ -1,4 +1,6 @@
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordLetters = /[a-zA-Z]/;
+const passwordNumbers = /[0-9]/;
 
 const required = (req, res, next) => {
   if (!req.body.email) {
@@ -9,6 +11,17 @@ const required = (req, res, next) => {
   }
   if (!req.body.password) {
     return res.status(400).send({ message: 'Password is required' });
+  }
+  if (req.body.password.length < 8) {
+    return res
+      .status(400)
+      .send({ message: 'Password must be at least 8 characters' });
+  }
+  if (req.body.password.search(passwordLetters) < 0
+  || req.body.password.search(passwordNumbers) < 0) {
+    return res
+      .status(400)
+      .send({ message: 'Password must contain letters and numbers' });
   }
   return next();
 };
